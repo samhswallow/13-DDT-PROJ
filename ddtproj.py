202dd3af80a6e755d505bb448589e1aa4a10c4b8
samhswallow <23639@student.macleans.school.nz>
2025-07-23 17:55:46 +1200

    5/06/25
    5/06/25

--- ddtproj.py
+++ ddtproj.py
@@ -1,8 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox
 from PIL import ImageTk, Image
+from lockedbrowser import launch_browser
+import subprocess
 
 
+url_list = []
+name_list = []
 
 
 def login_process():
@@ -24,9 +28,11 @@ def login_process():
 
    if password_check:
        main_menu()
-       messagebox.showinfo("Login Approved", "Approved!")
+       messagebox.showinfo("Login Approved", "Approved")
    else:
        messagebox.showerror("Login Denied")
+
+
 
 
 def save_password():
@@ -35,7 +41,7 @@ def save_password():
 
 
    if not username or not password:
-       messagebox.showerror("Error", "Username and password cannot be empty.")
+       messagebox.showerror("Error", "Username and password can't be empty.")
        return
 
 
@@ -72,3 +78,84 @@ def register():
    password_label.pack(pady=5)
    register_password_entry.pack(pady=5)
    save_button.pack(pady=10)
+  
+
+
+def create_tab():
+   create_tab_window = tk.Toplevel(root)
+   create_tab_window.title("Create Tab")
+   create_tab_window.geometry("300x200")
+
+
+   create_tab_label = tk.Label(create_tab_window, text="Enter URL")
+   create_tab_entry = tk.Entry(create_tab_window)
+
+
+   create_tab_label_name = tk.Label(create_tab_window, text="Enter URL's name")
+   create_tab_label_entry = tk.Entry(create_tab_window)
+
+
+   save_tab_button = tk.Button(create_tab_window, text="Save", command=save_tab, font=("Arial", 16))
+
+
+
+
+
+   create_tab_label.pack(pady=(10, 0))
+   create_tab_entry.pack(pady=(0, 10))
+   create_tab_label_name.pack(pady=(10, 0))
+   create_tab_label_entry.pack(pady=(0, 10))
+   save_tab_button.pack()
+   authorised_link_button.pack()
+
+
+
+def main_menu():
+   main_window = tk.Toplevel(root)
+   main_window.title("Register")
+   main_window.geometry("1600x1000")
+   main_window.configure(bg="black")
+
+
+ 
+   green_photo = Image.open("/Users/samswallow/Desktop/greensquare.png")
+   resized_photo = green_photo.resize((1500, 900))
+   green_background = ImageTk.PhotoImage(resized_photo)
+   green_background_label = tk.Label(main_window, image=green_background, bg="black")
+   green_background_label.place(x = 20, y=20)
+
+
+   create_link = tk.Button(main_window, text = "Create Link", command = create_tab, font = ("Arial", 16))
+   create_link.place(x= 700,y= 850)
+  
+   main_window.green_background = green_background
+
+
+
+
+root = tk.Tk()
+root.title("Login Window")
+root.geometry("300x200")
+
+
+username_label = tk.Label(root, text="Enter your username:")
+password_label = tk.Label(root, text="Enter your password:")
+
+
+username_entry = tk.Entry(root)
+password_entry = tk.Entry(root, show="*")
+
+
+login_button = tk.Button(root, text="Login", command=login_process)
+register_button = tk.Button(root, text="Register", command=register)
+
+
+username_label.pack(pady=5)
+username_entry.pack(pady=5)
+password_label.pack(pady=5)
+password_entry.pack(pady=5)
+login_button.pack(pady=5)
+register_button.pack(pady=5)
+
+
+root.mainloop()
--- lockedbrowser.py
+++ lockedbrowser.py
@@ -4,6 +4,8 @@ from PyQt5.QtCore import QUrl
 import sys
 
 
+
+
 global url
 
 
